f0c867cb8b686037b87ac623bd727da9fa15b190
MamounH <mhabukoush16@cit.just.edu.jo>
2021-08-09 20:55:05 +0300

    Spring Framework Course Initial Commit

---
+++ src/main/java/com/demo/spring/basics/demobasics/DemoBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring.basics.demobasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring/basics/demobasics/DemoBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring.basics.demobasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
